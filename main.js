77e0effba0a07b9a0a585bb1ed6daf4ee97fabfe
Sandysan1234 <140880265+Sandysan1234@users.noreply.github.com>
2024-07-01 18:50:18 +0700

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 
 const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.shadowMap.enabled = true; // Enable shadows
+renderer.shadowMap.enabled = true; 
 document.body.appendChild(renderer.domElement);
 
 const controls = new OrbitControls(camera, renderer.domElement);
